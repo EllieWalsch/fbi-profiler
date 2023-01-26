6da7dd8291e263056fbeaeab97632ef60cd38d3b
Joshua <janorris65@gmail.com>
2023-01-26 16:01:17 -0600

    create User routes and models

---
+++ models/Questionsmodel.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Question extends Model {};
+
+Question.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    question: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    category_id:{
+        type:DataTypes.INTEGER,
+        references:Category.id
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Question',
+  }
+);
+
+module.exports = Question;
---
+++ models/Subjectmodel.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Subject extends Model {};
+
+Subject.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    First_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Type_one_answers: {
+      type: DataTypes.STRING ? ARRAY ?,
+      allowNull: false,
+    },
+    Type_two_answers: {
+        type: DataTypes.STRING ? ARRAY ?,
+        allowNull: false,
+    },
+    Type_three_answers: {
+        type: DataTypes.STRING ? ARRAY ?,
+        allowNull: false,
+    },
+    Type_four_answers: {
+        type: DataTypes.STRING ? ARRAY ?,
+        allowNull: false,
+    },
+
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'subject',
+  }
+);
+
+module.exports = Subject;

884dbf75011b61dc3f76623305c44ed8d0ea1639
EllieWalsch <elliewalsch4@gmail.com>
2023-01-26 21:32:41 -0600

    Add login route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
-router.post("/", async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
 
@@ -33,7 +33,7 @@ router.post("/", async (req, res) => {
   // Logs an exisiting user in
 });
 
-router.post("/", async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const userData = await User.create(req.body);
 

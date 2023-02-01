af74b0102731057536923e2909e2b22316c74a98
NhatWin <nhat0933@gmail.com>
2023-01-31 21:53:26 -0600

    Helper no longer adds 0 to average

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,12 @@
 module.exports = {
     averageScore: (data) => {
-        const array = JSON.parse(data);
-        const total = array.reduce((acc, c) => acc + c, 0);
-        return Math.round(100*total/array.length)/100
+        let array = JSON.parse(data);
+        const arrayFix = array.filter(val => val !== 0)
+        const total = arrayFix.reduce((acc, c) => acc + c, 0);
+        if(isNaN(Math.round(100*total/arrayFix.length)/100)){
+            return 0
+        } else {
+            return Math.round(100*total/arrayFix.length)/100
+        }
     }
 }

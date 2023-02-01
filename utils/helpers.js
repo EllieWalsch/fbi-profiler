d26ee96cfa1c1160f19ccd6ed900f7f7a58d873e
EllieWalsch <elliewalsch4@gmail.com>
2023-01-31 21:52:11 -0600

    Bug fix: confirm data in averageScore helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,9 @@
 module.exports = {
     averageScore: (data) => {
-        const array = JSON.parse(data);
-        const total = array.reduce((acc, c) => acc + c, 0);
-        return Math.round(100*total/array.length)/100
+        if (data) {
+            const array = JSON.parse(data);
+            const total = array.reduce((acc, c) => acc + c, 0);
+            return Math.round(100*total/array.length)/100
+        }
     }
 }

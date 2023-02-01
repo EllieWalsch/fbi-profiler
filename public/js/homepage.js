13b0fdafcddd18f06c63f7ac6a1782232faea9e5
Joshua <janorris65@gmail.com>
2023-02-01 00:15:43 -0600

    add + question button in homepage hndle and js elements in homepagejs

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -4,4 +4,9 @@ addSubject.addEventListener('click', () => {
     document.location.replace("/add-subject")
 })
 
+const addQuestion = document.querySelector("#addQuestion");
+
+addQuestion.addEventListener('click', () => {
+    document.location.replace("/add-question")
+})
 

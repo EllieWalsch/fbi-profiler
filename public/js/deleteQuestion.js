a6ed05199bcdfd723434f8684fa00e7c86ee7bad
Joshua <janorris65@gmail.com>
2023-02-06 14:33:02 -0600

    create data if statement to check for type and notnull to not crash fetch delete

--- public/js/deleteQuestion.js
+++ public/js/deleteQuestion.js
@@ -4,8 +4,14 @@ deleteBtn.addEventListener("click", (event)=> {
     event.preventDefault();
     console.log(event.target.getAttribute("data-id"))
     let questionId = event.target.getAttribute("data-id");
+    let questionIdValue =typeof parseInt(questionId);
+    console.log( typeof parseInt(questionId))
+    if (questionIdValue === 'number' &&  questionId !== null ){
     fetch(`/api/question/${questionId}`, {
         method: "DELETE",
       })
       .then(() => document.location.replace("/"));
+} else {
+    return
+}
 })
